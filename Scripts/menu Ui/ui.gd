0a6fc95b41063ac8768a80e6fdfddecfe11d06ac
Mike3DGit <156416721+Mike3DGit@users.noreply.github.com>
2025-06-24 22:39:26 +0300

    new change

--- Scripts/menu Ui/ui.gd
+++ Scripts/menu Ui/ui.gd
@@ -7,7 +7,7 @@ func _on_new_game_button_pressed() -> void:
 
 
 func _on_credits_button_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Scenes/Credits/credits.tscn")
 
 
 func _on_quit_button_pressed() -> void:
